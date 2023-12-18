ace37666f9dff548bbebcd84ceab9878b4f93a61
LuannLopes <luanlopesofc@gmail.com>
2023-12-17 22:30:51 -0300

    feat: create initial logic to fetch the api

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./index.css";
 import { Button, Input } from "../../components";
+import { SAVE_USERNAME_PATH } from "../../services/constants";
 
 export const HomeScreen = () => {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ export const HomeScreen = () => {
       alert("UserName deve conter mais do que 3 caracteres!");
       return;
     }
-    localStorage.setItem("@supermarket-list:userName", userName);
+    localStorage.setItem(SAVE_USERNAME_PATH, userName);
     navigate("/list");
   };
 
