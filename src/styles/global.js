e3c0fc3a9675da0387436e3056b95e0f707819ab
LuannLopes <luanlopesofc@gmail.com>
2024-01-10 16:13:01 -0300

    feat: Global Styles and Media Query

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,63 +1,68 @@
-@font-face {
-  font-family: 'Avenir Next';
-  src: url('/public/fonts/AvenirNext-Regular.otf');
-}
+import { createGlobalStyle } from 'styled-components'
 
-@font-face {
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
   font-family: 'Avenir Next';
-  src: url('/public/fonts/AvenirNext-Bold.otf');
+  src: url('fonts/AvenirNext-Regular.otf');
+  }
+
+  @font-face {
+  font-family: 'Avenir Next';
+  src: url('fonts/AvenirNext-Bold.otf');
   font-weight: bold;
-}
+  }
 
-@font-face {
+  @font-face {
   font-family: 'Avenir Next';
-  src: url('/public/fonts/AvenirNext-It.otf');
+  src: url('fonts/AvenirNext-It.otf');
   font-style: italic;
-}
+  }
 
-@font-face {
+  @font-face {
   font-family: 'Avenir Next';
-  src: url('/public/fonts/AvenirNext-Medium.ttf');
+  src: url('fonts/AvenirNext-Medium.ttf');
   font-weight: 500;
-}
+  }
 
-body {
+  body {
   margin: 0;
   font-family: 'Avenir Next';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
+  }
 
-code {
+  code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
-}
+  }
 
-h1 {
+  h1 {
   color: #000;
   font-size: 24px;
   font-weight: bold;
   line-height: 22px;
-}
+  }
 
-h3 {
+  h3 {
   font-size: 16px;
   font-weight: 500;
   margin: 0;
   padding: 0;
-}
-
-@media screen and (max-width: 420px) {
-  h1 {
-    font-size: 18px;
   }
 
-  h3 {
-    font-size: 12px;
-  }
-}
-
-::-webkit-scrollbar {
+  ::-webkit-scrollbar {
   visibility: hidden;
   width: 0;
-}
+  }
+
+  @media (max-width: 420px) {
+    h1 {
+    font-size: 18px;
+    }
+
+    h3 {
+    font-size: 12px;
+    }
+  }
+
+`
